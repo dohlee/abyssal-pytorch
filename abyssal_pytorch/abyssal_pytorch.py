1ae4e2f908fa19035f9e80240b4af8541b486797
dohlee <dohlee.bioinfo@gmail.com>
2023-02-12 13:16:10 +0900

    Implement ESM2 embedding.

--- abyssal_pytorch/abyssal_pytorch.py
+++ abyssal_pytorch/abyssal_pytorch.py
@@ -1,7 +1,14 @@
+import esm
 import torch
+import torch.nn.functional as F
+
 from torch import nn, einsum
 from einops import rearrange
-import torch.nn.functional as F
+
+def mutate(seq, pos, aa_mut):
+    seq = list(seq)
+    seq[pos - 1] = aa_mut  # `pos` is 1-based.
+    return ''.join(seq)
 
 class LightAttention(nn.Module):
     def __init__(self):
@@ -15,8 +22,10 @@ class LightAttention(nn.Module):
 class Abyssal(nn.Module):
     def __init__(self):
         super().__init__()
+        self.embedder, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+        self.batch_converter = alphabet.get_batch_converter()
+        
         self.light_attention = LightAttention()
-
         self.fc_block = nn.Sequential(
             nn.Linear(2560, 2048),
             nn.ReLU(),  # Not sure
@@ -25,17 +34,34 @@ class Abyssal(nn.Module):
             nn.Linear(1024, 1),
         )
 
-    def forward(self, x1, x2):
-        x1, x2 = map(self.light_attention, [x1, x2])
-        x = torch.cat([x1, x2], dim=-1)
-        x = rearrange(x, 'b c d -> b (c d)')
+    def forward(self, seq, pos, aa_mut):
+        bsz = len(seq)
+        seq_orig, seq_mut = seq, map(mutate, seq, pos, aa_mut)
+
+        data = [('seq_orig', o) for o in seq_orig] + [('seq_mut', m) for m in seq_mut]
+        batch_labels, batch_strs, batch_tokens = self.batch_converter(data)
+        
+        with torch.no_grad():
+            results = self.embedder(batch_tokens, repr_layers=[33], return_contacts=False)
+
+        x_orig = results['representations'][33][:bsz][range(bsz), pos-1].unsqueeze(1)
+        x_mut = results['representations'][33][bsz:][range(bsz), pos-1].unsqueeze(1)
+
+        x_orig, x_mut = map(self.light_attention, [x_orig, x_mut])
+        x = torch.cat([x_orig, x_mut], dim=-1)
+        x = rearrange(x, 'b c h -> b (c h)')
 
         return self.fc_block(x)
 
 if __name__ == '__main__':
-    x1 = torch.randn([16, 1, 1280])
-    x2 = torch.randn([16, 1, 1280])
+    seq = [
+        'MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG',
+        'KALTARQQEVFDLIRDHISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE',
+    ]
+    pos = torch.tensor([10, 11]).long()
+    aa_mut = ['W', 'W']
 
     model = Abyssal()
-    x = model(x1, x2)
+    x = model(seq, pos, aa_mut)
+
     print(x.shape)
