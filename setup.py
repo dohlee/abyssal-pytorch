ea16b6d533b1046bcaa8c7c27ecc321f328a8492
dohlee <dohlee.bioinfo@gmail.com>
2023-02-18 21:08:31 +0900

    0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'abyssal-pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'Abyssal - Pytorch',
   author = 'Dohoon Lee',
